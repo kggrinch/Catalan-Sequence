2062ec28d3ab933e8dcec81915cac0319b6da00a
Kirill Grichanichenko <kirillkongrichanichenko@gmail.com>
2024-12-29 14:36:36 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ Input Validation Explanation - This function returns an integer. Although, a lon
 an integer, resulting in me choosing to return an integer value. With the integer value, I am able to set a maximum possible nth value of 19, where further nth values would cause 
 integer overflow. However, by choosing to return an integer with these bounds, I can still demonstrate the recursion process to calculate the Catalan sequence.
 
-If needed for larger calculations, the program could be modified to return long, but it might be better to calculate the Catalan using a different approach for larger calculation.
+If needed for larger calculations, the program could be modified to return long, but it might be better to calculate the Catalan using a different approach for larger calculations.
 */ 
 int catalan(int n)
 {
